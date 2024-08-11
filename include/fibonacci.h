7ec6ee046c80643df394f07c50e32f11538da13c
JaehoonSong12 <manual20151276@gmail.com>
2024-08-10 23:51:47 -0400

    08/10/2024 updated by Jaehoon

--- include/fibonacci.h
+++
@@ -1,6 +0,0 @@
-#ifndef FIBONACCI_H
-#define FIBONACCI_H
-
-void display_fibonacci(int n);
-
-#endif
--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "bash_compiler.h"
-#include "fibonacci.h"
-
-int main() {
-    // Your code here
-    sayHello();
-    display_fibonacci(10);
-    return 0;
-}
